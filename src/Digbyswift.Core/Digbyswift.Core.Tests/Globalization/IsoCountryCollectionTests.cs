90ef6fe7c75a6417ecb12fa0e6aabea6fc45c6d5
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2023-07-03 12:21:03 +0100

    Add tests for DateTimeComparerm ListExtensions and IsoCountry classes

---
+++ src/Digbyswift.Core/Digbyswift.Core.Tests/Globalization/IsoCountryCollectionTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Digbyswift.Core.Globalization;
+using NUnit.Framework;
+
+namespace Digbyswift.Core.Tests.Globalization
+{
+    [TestFixture]
+    public class IsoCountryCollectionTests
+    {
+        
+        [Test]
+        public void Items_HasConstantCountValue()
+        {
+            // Arrange
+            const int expectedCountryCount = 249;
+            
+            // Act
+            var countryCount = IsoCountryCollection.Items.Count;
+            
+            // Assert
+            Assert.That(countryCount, Is.EqualTo(expectedCountryCount));
+        }
+        
+        [Test]
+        public void Items_IsReadonly()
+        {
+            // Assert
+            Assert.That(IsoCountryCollection.Items is IReadOnlyCollection<IsoCountry>, Is.True);
+        }
+        
+   }
+}
