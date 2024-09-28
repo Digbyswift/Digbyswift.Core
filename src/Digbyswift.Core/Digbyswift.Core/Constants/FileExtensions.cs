95b6337fb5527064c0928372651d5b8c3f957644
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2024-09-28 16:27:11 +0100

    Add file extension constants

---
+++ src/Digbyswift.Core/Digbyswift.Core/Constants/FileExtensions.cs
@@ -0,0 +1,86 @@
+﻿namespace Digbyswift.Core.Constants;
+
+public static class FileExtensions
+{
+    // Images
+    public const string Bmp = ".bmp";
+    public const string Eps = ".eps";
+    public const string Gif = ".gif";
+    public const string Jpeg = ".jpeg";
+    public const string Jpg = ".jpg";
+    public const string Png = ".png";
+    public const string Psd = ".psd";
+    public const string Svg = ".svg";
+    public const string Tif = ".tif";
+    public const string Tiff = ".tiff";
+
+    // Documents
+    public const string Csv = ".csv";
+    public const string Doc = ".doc";
+    public const string Docx = ".docx";
+    public const string Pdf = ".pdf";
+    public const string Ppt = ".ppt";
+    public const string Pptx = ".pptx";
+    public const string Rtf = ".rtf";
+    public const string Txt = ".txt";
+    public const string Xls = ".xls";
+    public const string Xlsx = ".xlsx";
+
+    // Compression
+    public const string Cab = ".cab";
+    public const string Jar = ".jar";
+    public const string Zip = ".zip";
+    public const string Zip7 = ".7z";
+    public const string Rar = ".rar";
+
+    // Code
+    public const string Asp = ".asp";
+    public const string Aspx = ".aspx";
+    public const string Bat = ".bat";
+    public const string Config = ".config";
+    public const string Cs = ".cs";
+    public const string Cshtml = ".cshtml";
+    public const string Csproj = ".csproj";
+    public const string Css = ".css";
+    public const string Dart = ".dart";
+    public const string Dll = ".dll";
+    public const string Exe = ".exe";
+    public const string Htm = ".htm";
+    public const string Html = ".html";
+    public const string Js = ".js";
+    public const string Jsx = ".jsx";
+    public const string Json = ".json";
+    public const string Mdb = ".mdb";
+    public const string Md = ".md";
+    public const string Nupkg = ".nupkg";
+    public const string Pbd = ".pbd";
+    public const string Ps1 = ".ps1";
+    public const string Sass = ".sass";
+    public const string Scss = ".scss";
+    public const string Sln = ".sln";
+    public const string Sql = ".sql";
+    public const string Ts = ".ts";
+    public const string Tsx = ".tsx";
+    public const string Xml = ".xml";
+    public const string Xsl = ".xsl";
+    public const string Yml = ".yml";
+
+    // Fonts
+    public const string Eot = ".eot";
+    public const string Otf = ".otf";
+    public const string Ttf= ".ttf";
+    public const string Woff = ".woff";
+    public const string Woff2 = ".woff2";
+
+    // Media
+    public const string Aac = ".aac";
+    public const string Avi = ".avi";
+    public const string Flv = ".flv";
+    public const string M4a = ".m4a";
+    public const string Mov = ".mov";
+    public const string Mp3 = ".mp3";
+    public const string Mp4 = ".mp4";
+    public const string Mpeg = ".mpeg";
+    public const string Mpg = ".mpg";
+    public const string Wav = ".wav";
+}
