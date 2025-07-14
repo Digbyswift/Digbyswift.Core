1d4934aad3cad54342483d4b4cc9c81c33a3a83a
Joseph Earnshaw <joe@digbyswift.com>
2025-07-14 10:19:09 +0100

    Adding StringCompressionExtensionsTests

---
+++ src/Digbyswift.Core/Digbyswift.Core.Tests/Extensions/StringExtensions/StringCompressionExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿using Digbyswift.Core.Extensions;
+using NUnit.Framework;
+
+namespace Digbyswift.Core.Tests.Extensions.StringExtensions;
+
+[TestFixture]
+public class StringCompressionExtensionsTest
+{
+    [Test]
+    public void Compress_ReturnsCompressedString_WhenGivenAnUncompressedString()
+    {
+        // Arrange
+        var value = "Testing a string with spaces";
+        var expectedResult = "H4sIAAAAAAAECgtJLS7JzEtXSFQoLikCMcozSzIUigsSk1OLAQgrK2QcAAAA";
+
+        // Act
+        var result = value.Compress();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void DeCompress_ReturnsUncompressedString_WhenGivenCompressedString()
+    {
+        // Arrange
+        var value = "H4sIAAAAAAAECgtJLS7JzEtXSFQoLikCMcozSzIUigsSk1OLAQgrK2QcAAAA";
+        var expectedResult = "Testing a string with spaces";
+
+        // Act
+        var result = value.Decompress();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
