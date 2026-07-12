8a0efc6bc2803b70f08450f24cb0c3d9feebee10
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2026-07-12 19:16:37 +0100

    Update hashes

--- src/Digbyswift.Core/Extensions/Encryption/ShaExtensions.cs
+++ src/Digbyswift.Core/Extensions/Encryption/ShaExtensions.cs
@@ -8,13 +8,27 @@ public static class ShaExtensions
 {
     public static string ToSHA1Hash(this string text)
     {
+#pragma warning disable S4790
         using var algorithm = SHA1.Create();
+#pragma warning restore S4790
         return algorithm.GenerateHashString(text);
     }
 
     public static string ToSHA256Hash(this string text)
     {
         using var algorithm = SHA256.Create();
+        return algorithm.GenerateHashString(text);
+    }
+
+    public static string ToSHA384Hash(this string text)
+    {
+        using var algorithm = SHA384.Create();
+        return algorithm.GenerateHashString(text);
+    }
+
+    public static string ToSHA512Hash(this string text)
+    {
+        using var algorithm = SHA512.Create();
         return algorithm.GenerateHashString(text);
     }
 
